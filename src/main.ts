c9966843495769d6abb0c1b5d2d34cec37e19d98
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-10 18:24:53 +0600

    Реализован шаблон для наглядности

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import Player from './classes/Player';
 //Управление
 const ctrlPlay: HTMLButtonElement = <HTMLButtonElement>document.getElementById('ctrlPlay');
 const ctrlPause: HTMLButtonElement = <HTMLButtonElement>document.getElementById('ctrlPause');
@@ -7,18 +8,25 @@ const ctrlToggleBitrate: HTMLButtonElement = <HTMLButtonElement>document.getElem
 const viewCurrentBitrate: HTMLElement = <HTMLElement>document.getElementById('viewCurrentBitrate');
 const viewCurrentStatus: HTMLElement = <HTMLElement>document.getElementById('viewCurrentStatus');
 
+const player = new Player({
+	high: 'http://bfmstream.bfm.ru:8004/fm64',
+	low: 'http://bfmstream.bfm.ru:8004/fm32'
+}, 3, function(data){viewCurrentStatus.innerText = data}, function(data){viewCurrentBitrate.innerText = data;})
 ctrlPlay.addEventListener('click', ()=>{
 	ctrlPlay.disabled = true;
 	ctrlPause.disabled = false;
 	ctrlToggleBitrate.disabled = false;
+	player.play();
 })
 
 ctrlPause.addEventListener('click', ()=>{
 	ctrlPlay.disabled = false;
 	ctrlPause.disabled = true;
 	ctrlToggleBitrate.disabled = true;
+	player.pause();
 })
 
 ctrlToggleBitrate.addEventListener('click', ()=>{
-	
+	player.toggleBitrate();
 })
+
