79a750373c48f559f7e75c901176bf440f5e7332
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-10 11:42:34 +0600

    Добавлено управление

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import './style.css'
+//Управление
+const ctrlPlay: HTMLButtonElement = <HTMLButtonElement>document.getElementById('ctrlPlay');
+const ctrlPause: HTMLButtonElement = <HTMLButtonElement>document.getElementById('ctrlPause');
+const ctrlToggleBitrate: HTMLButtonElement = <HTMLButtonElement>document.getElementById('ctrlToggleBitrate');
+//визульное отображение
+const viewCurrentBitrate: HTMLElement = <HTMLElement>document.getElementById('viewCurrentBitrate');
+const viewCurrentStatus: HTMLElement = <HTMLElement>document.getElementById('viewCurrentStatus');
+
+ctrlPlay.addEventListener('click', ()=>{
+	ctrlPlay.disabled = true;
+	ctrlPause.disabled = false;
+	ctrlToggleBitrate.disabled = false;
+})
+
+ctrlPause.addEventListener('click', ()=>{
+	ctrlPlay.disabled = false;
+	ctrlPause.disabled = true;
+	ctrlToggleBitrate.disabled = true;
+})
+
+ctrlToggleBitrate.addEventListener('click', ()=>{
+	
+})
